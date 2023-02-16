c5a7d570318eecef314a2d8dac3f817837d83fc3
Ayoub <aragrag.ayoub@gmail.com>
2023-02-16 00:37:50 -0400

    Modification dans le fichier main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include<CableLAN.h>
 
-CableLAN cable;
+CableLAN cable; // instancier l'Obket CableLAN
 
 void setup() {
   // Configuration des broches en entrée
   cable.init_congig_broches(); 
+
   Serial.begin(9600);
 }
 
@@ -13,7 +14,7 @@ void loop() {
   // Lecture des signaux sur les broches
   cable.read_signals();
 
-  // Afficher type de cable
+  // Afficher type de cable sur Monitor
   cable.cable_type(); 
   delay(10000);
 }
